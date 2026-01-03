f4967c148b2ec47e27f06409ea1c171679494de9
filtcgil <archiviofiltcgil@gmail.com>
2026-01-03 11:07:05 +0100

    Create MainApplication.java

---
+++ android/app/src/main/java/com/unionhubapp/MainApplication.java
@@ -0,0 +1,7 @@
+package com.unionhubapp;
+
+import android.app.Application;
+import com.facebook.react.ReactApplication;
+
+public class MainApplication extends Application implements ReactApplication {
+}
