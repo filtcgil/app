f6e734657a3985df423c3c264b59358d681e807c
filtcgil <archiviofiltcgil@gmail.com>
2026-01-03 18:25:33 +0100

    Create AnnunciScreen.tsx

---
+++ screens/AnnunciScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AnnunciScreen() {
+  return (
+    <View>
+      <Text>Annunci sindacali</Text>
+    </View>
+  );
+}
