61103c5c78f80e162b8d82875785218f959f09eb
filtcgil <archiviofiltcgil@gmail.com>
2026-01-03 18:26:05 +0100

    Create ScioperiScreen.tsx

---
+++ screens/ScioperiScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function ScioperiScreen() {
+  return (
+    <View>
+      <Text>Scioperi e mobilitazioni</Text>
+    </View>
+  );
+}
