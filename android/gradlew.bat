7f97aac0bf81335e9733f5858644ff47fad172df
filtcgil <archiviofiltcgil@gmail.com>
2026-01-04 16:26:12 +0100

    Update gradlew.bat

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,23 +1,26 @@
 @echo off
 setlocal
 
+rem --- Impostazioni di base ---
 set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
+if "%DIRNAME%"=="" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+rem --- Rimuoviamo le virgolette dai parametri JVM ---
+set DEFAULT_JVM_OPTS=-Xmx1024m -Xms64m
 
+rem --- Percorso del wrapper Gradle ---
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
+rem --- Comando Java ---
 if defined JAVA_HOME (
-  set JAVACMD=%JAVA_HOME%\bin\java.exe
+    set JAVACMD=%JAVA_HOME%\bin\java.exe
 ) else (
-  set JAVACMD=java
+    set JAVACMD=java
 )
 
-"%JAVACMD%" %DEFAULT_JVM_OPTS% ^
-  -classpath "%CLASSPATH%" ^
-  org.gradle.wrapper.GradleWrapperMain %*
+rem --- Esecuzione del wrapper ---
+"%JAVACMD%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 endlocal
