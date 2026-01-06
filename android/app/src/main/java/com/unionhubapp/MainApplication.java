3ba555048c6a6c7cd4361b00e8a111c229cda66a
filtcgil <archiviofiltcgil@gmail.com>
2026-01-07 00:26:22 +0100

    Update MainApplication.java

--- android/app/src/main/java/com/unionhubapp/MainApplication.java
+++ android/app/src/main/java/com/unionhubapp/MainApplication.java
@@ -1,7 +1,48 @@
 package com.unionhubapp;
 
 import android.app.Application;
+
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
+
+import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new DefaultReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Qui puoi aggiungere eventuali package manuali
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      DefaultNewArchitectureEntryPoint.load();
+    }
+  }
 }
