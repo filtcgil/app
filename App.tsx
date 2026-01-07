039b1d0bad8da401bceea3239c5b9d4bdba5a709
filtcgil <archiviofiltcgil@gmail.com>
2026-01-07 15:14:04 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,23 +5,27 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import FolderScreen from './src/screens/FolderScreen';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  Folder: { title?: string };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: 'UnionHub' }}
         />
-
         <Stack.Screen
           name="Folder"
           component={FolderScreen}
-          options={({ route }: any) => ({
-            title: route.params?.title ?? 'Cartella'
+          options={({ route }) => ({
+            title: route.params?.title ?? 'Cartella',
           })}
         />
       </Stack.Navigator>
