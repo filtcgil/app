b164ab7ad6cf00b26752730fa74fb6930be83fec
filtcgil <archiviofiltcgil@gmail.com>
2026-01-07 09:30:55 +0100

    Update MainApplication.java

--- android/app/src/main/java/com/unionhub/app/MainApplication.java
+++ android/app/src/main/java/com/unionhub/app/MainApplication.java
@@ -1,21 +1,53 @@
 package com.unionhub.app;
 
-import android.os.Bundle;
+import android.app.Application;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactHost;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultReactHost;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
+import java.util.Collections;
+import java.util.List;
 
-public class MainActivity extends ReactActivity {
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new DefaultReactNativeHost(this) {
+
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return true;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          return Collections.emptyList();
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
 
   @Override
-  protected String getMainComponentName() {
-    return "UnionHub";
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
   }
 
   @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName());
+  public ReactHost getReactHost() {
+    return DefaultReactHost.getDefaultReactHost(
+        getApplicationContext(),
+        mReactNativeHost
+    );
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
   }
 }
