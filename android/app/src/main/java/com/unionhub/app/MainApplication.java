2b2e8b60ff0c9984265dfa0891c184f2e1c20afc
filtcgil <archiviofiltcgil@gmail.com>
2026-01-12 15:55:30 +0100

    Update MainApplication.java

--- android/app/src/main/java/com/unionhub/app/MainApplication.java
+++ android/app/src/main/java/com/unionhub/app/MainApplication.java
@@ -1,40 +1,34 @@
 package com.unionhub.app;
 
 import android.app.Application;
-
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.PackageList;
-
 import java.util.List;
-
-// IMPORT FONDAMENTALE
-import com.unionhub.app.BuildConfig;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost =
+        new ReactNativeHost(this) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+                return BuildConfig.DEBUG;
+            }
 
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+            @Override
+            protected List<ReactPackage> getPackages() {
+                return new PackageList(this).getPackages();
+            }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          return new PackageList(this).getPackages();
-        }
+            @Override
+            protected String getJSMainModuleName() {
+                return "index";
+            }
+        };
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
-      };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
 }
