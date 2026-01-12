e64cecbb93533931550fa22b6402935ecdd161b5
filtcgil <archiviofiltcgil@gmail.com>
2026-01-12 11:40:29 +0100

    Update MainApplication.java

--- android/app/src/main/java/com/unionhub/app/MainApplication.java
+++ android/app/src/main/java/com/unionhub/app/MainApplication.java
@@ -4,18 +4,25 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactHost;
-import com.facebook.react.defaults.DefaultReactNativeHost;
-import com.facebook.react.defaults.DefaultReactHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.PackageList;
+
+import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new DefaultReactNativeHost(this) {
+      new ReactNativeHost(this) {
 
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          return packages;
         }
 
         @Override
@@ -27,14 +34,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
-  }
-
-  @Override
-  public ReactHost getReactHost() {
-    return DefaultReactHost.getDefaultReactHost(
-        getApplicationContext(),
-        mReactNativeHost
-    );
   }
 
   @Override
