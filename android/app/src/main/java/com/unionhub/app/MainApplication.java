5b81799b5d0a96129b2d027f5809758615c7363c
filtcgil <archiviofiltcgil@gmail.com>
2026-01-12 11:26:00 +0100

    Update MainApplication.java

--- android/app/src/main/java/com/unionhub/app/MainApplication.java
+++ android/app/src/main/java/com/unionhub/app/MainApplication.java
@@ -4,9 +4,13 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactHost;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.defaults.DefaultReactHost;
+
+import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -16,6 +20,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         public boolean getUseDeveloperSupport() {
           return true;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          return new PackageList(this).getPackages();
         }
 
         @Override
