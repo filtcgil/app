3a3148a67b81696f7f594ffc5e3afacac76c2238
filtcgil <archiviofiltcgil@gmail.com>
2026-01-13 19:15:50 +0100

    Create DriveService.js

---
+++ src/services/DriveService.js
@@ -0,0 +1,8 @@
+// Placeholder per l'integrazione con Google Drive
+export async function getDriveFolders() {
+  // TODO: chiamata API Drive con permessi in base all'utente
+  return [
+    { id: '1', name: 'Documenti Azienda' },
+    { id: '2', name: 'Procedure' }
+  ];
+}
