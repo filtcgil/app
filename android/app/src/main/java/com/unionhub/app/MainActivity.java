fa67e5ebc24f2c4da418f20909c24ef99eaed781
filtcgil <archiviofiltcgil@gmail.com>
2026-01-13 19:17:47 +0100

    Update MainActivity.java

--- android/app/src/main/java/com/unionhub/app/MainActivity.java
+++ android/app/src/main/java/com/unionhub/app/MainActivity.java
@@ -1,11 +1,22 @@
 package com.unionhub.app;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "UnionHubApp";
+    }
 
-  @Override
-  protected String getMainComponentName() {
-    return "UnionHubApp";
-  }
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
+            false,
+            false
+        );
+    }
 }
