76659b43d0540224920ae08ed297a7975d029ae4
filtcgil <archiviofiltcgil@gmail.com>
2026-01-14 04:21:25 +0100

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 
-export default function LoginScreen({ navigation }) {
+const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // TODO: integrare Firebase Auth e permessi
-    navigation.replace('Home');
+    // Autenticazione tramite backend
   };
 
   return (
-    <View style={{ flex:1, justifyContent:'center', padding:20 }}>
-      <Text>Email</Text>
-      <TextInput value={email} onChangeText={setEmail} />
-      <Text>Password</Text>
-      <TextInput value={password} onChangeText={setPassword} secureTextEntry />
+    <View>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
       <Button title="Login" onPress={handleLogin} />
-      <Button title="Registrati" onPress={() => navigation.navigate('Register')} />
     </View>
   );
-  }
+};
+
+export default LoginScreen;
